9dd5af60886dbb89e061935ce48f3672440f984b
Trish Ogen <patriciaogen@gmail.com>
2019-05-31 22:20:46 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,7 +3,8 @@ class Person
                 :weight, :handed, :complexion, :t_shirt_size,
                 :wrist_size, :glove_size, :pant_length, :pant_width
 
-  def initialize(hash)
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
   end
-  
+
 end
